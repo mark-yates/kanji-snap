fb7af66fdbf18dd064e09fc97d175ad00cdedb0b
mark-yates <mark.yates@gmail.com>
2025-12-31 00:15:41 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,37 @@
-const CACHE_NAME = "kanji-snap-vNEXT1"; // bump this when you change files
+const CACHE_NAME = "kanji-snap-v8";
 
 const ASSETS = [
   "./",
   "./index.html",
-  "./manifest.webmanifest",
   "./sw.js",
-  "./fonts/my-font.ttf",
+  "./manifest.webmanifest",
+  "./apple-touch-icon.png",
   "./icon-192.png",
   "./icon-512.png",
-  "./apple-touch-icon.png",
+  "./fonts/my-font.ttf",
 
-  // Grade data (add more later)
+  // Grade data
   "./grade-1.json",
   "./grade-2.json",
   "./grade-3.json",
 ];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
