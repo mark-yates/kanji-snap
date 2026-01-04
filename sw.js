fa3f552ffc0103df0501eccdc92963a394cef2b6
mark-yates <mark.yates@gmail.com>
2026-01-04 08:42:29 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kanji-snap-v30";
+const CACHE_NAME = "kanji-snap-v30.1";
 
 const ASSETS = [
   "./",
@@ -47,3 +47,4 @@ self.addEventListener("fetch", event => {
     caches.match(event.request).then(cached => cached || fetch(event.request))
   );
 });
+
