11f4dec5ab22cf35c42904dddb42f7136cf8a9a9
mark-yates <mark.yates@gmail.com>
2026-01-16 20:34:16 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kanji-snap-shell-v51.5";
+const CACHE_NAME = "kanji-snap-shell-v51.6";
 const RUNTIME_CACHE = "kanji-snap-runtime-v1";
 
 const ASSETS = [
@@ -113,6 +113,7 @@ self.addEventListener("fetch", (event) => {
     }
   })());
 });
+
 
 
 
