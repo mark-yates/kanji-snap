7cc94b226a1a0bc8a7673faf3ac99a00507f6cf6
mark-yates <mark.yates@gmail.com>
2026-02-13 10:32:41 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* sw.js */
 
-const CACHE_NAME = "kanji-snap-cache-v1.75.3";      // bump for app shell updates
+const CACHE_NAME = "kanji-snap-cache-v1.75.4";      // bump for app shell updates
 const RUNTIME_CACHE = "kanji-snap-runtime-v1"; // must match settings.js/game-quiz.js
 
 
